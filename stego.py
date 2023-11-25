080d034043a75ba3d34acb875fb79673c9692f4e
swarnabo <151909574+swarnabo@users.noreply.github.com>
2023-11-24 23:21:48 -0800

    Add files via upload

---
+++ stego.py
@@ -0,0 +1,35 @@
+import cv2
+import os
+import string
+img = cv2.imread("mypic.jpg")
+msg = input ("Enter secret message:")
+password = input("Enter a passcode:")
+d = {}
+c = {}
+for i in range(255):
+    d[chr(i)] = i;
+    c[i] = chr(i)
+m = 0
+n = 0
+z = 0
+for i in range(len(msg)):
+    img[n,m,z] = d[msg[i]]
+    n = n+1
+    m = m+1
+    z = (z+1)%3
+cv2.imwrite("encryptedImage.jpg",img)
+os.system("start encryptedImage.jpg")
+message = ""
+n = 0
+m = 0
+z = 0
+pas = input("Enter passcode for decryption:")
+if password == pas:
+    for i in range(len(msg)):
+        message = message + c[img[n,m,z]]
+        n = n+1
+        m = m+1
+        z = (z+1)%3
+    print("Decrytion message:",message)
+else:
+    print("you are not auth")
